dc7f8a8588725f9fc55026ea8ff76135b19d6e56
DJ Neill <dj.neill856@gmail.com>
2025-11-03 12:31:14 -0600

    robots no index

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="robots" content="noindex, nofollow" />
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=Roboto+Slab:wght@300;400;500;600;700;800;900&display=swap&family=Merienda:wght@300..900&display=swap"
           rel="stylesheet"
